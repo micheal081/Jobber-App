0f308f6e2e4eeaadc57930e510ee73c029859d8b
micheal081 <michig3000@gmail.com>
2023-08-04 22:38:35 -0700

    initial commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Text>Home</Text>
+      <Text>I love you</Text>
+    </View>
+  );
+};
+
+export default Home;
