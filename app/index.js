3cbd31ea5200c705f0e32d352cbc0b11ece38b4b
micheal081 <michig3000@gmail.com>
2023-08-04 22:56:58 -0700

    added components and other constants

--- app/index.js
+++ app/index.js
@@ -1,11 +1,24 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+// Constants
+import { COLORS, SIZES, icons, images } from "../constants";
+
+// Components
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
   return (
     <View>
       <Text>Home</Text>
       <Text>Home</Text>
-      <Text>I love you</Text>
+      <Text>I love you okay</Text>
     </View>
   );
 };
